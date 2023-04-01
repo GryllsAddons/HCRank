2e15f3b095d117c857b13b5e56dd3f10fac8dda8
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2023-04-01 16:25:11 +0100

    Add files via upload

--- MobWarn.lua
+++ MobWarn.lua
@@ -1,21 +1,30 @@
 MobWarn = CreateFrame("Frame", nil, UIParent)	
 MobWarn:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
 
-local function color(deaths)
-  if deaths >= 50 then
-    return "f44336"
-  elseif deaths >= 25 then
-    return "ff8000"
-  else
-    return "ffff00"
+local function colorRank(num)
+  -- https://colorkit.co/gradient-palette/ff0000-f7ef00/?steps=4
+  local color = "ffffff"
+  if num >= 75 then
+    color = "f7ef00"
+  elseif num >= 50 then
+    color = "faa000"    
+  elseif num >= 25 then
+    color = "fc5100"    
+  elseif num >= 1 then
+    color = "ff0000"
   end
+  return color
 end
 
 MobWarn:SetScript("OnEvent", function()
     local name = UnitName("mouseover")
-    local deaths = MobWarn.mobs[name]    
-    if deaths then
-      GameTooltip:AddLine("|cff" .. color(deaths) .. "Players killed: " .. deaths .."|r")   
+    local mob = MobWarn.mobs[name]
+    if mob then
+      local rank = MobWarn.mobs[name]["Rank"]
+      local kills = MobWarn.mobs[name]["Kills"]
+      local color = colorRank(rank)
+      GameTooltip:AddLine("|cff" .. color .. "Players Killed: " .. kills .. "|r")
+      GameTooltip:AddLine("|cff" .. color .. "Mob Rank: " .. rank .. "|r")
       GameTooltip:Show()
     end
 end)
